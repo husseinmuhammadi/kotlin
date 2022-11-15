84d9179c3760488650fe2d6bcef10c8fee35a17a
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-11-15 09:12:18 +0100

    Data class in kotlin

--- kotlin-basics/src/test/kotlin/org/tutorial/kotlin/model/UserTest.kt
+++ kotlin-basics/src/test/kotlin/org/tutorial/kotlin/model/UserTest.kt
@@ -4,9 +4,25 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class UserTest {
+
+    private val user = User("hmohammadi", "123456")
+
     @Test
     fun `create object from data class`() {
-        val user = User("hmohammadi", "123456")
         println("user = $user")
+    }
+
+    @Test
+    fun `copy data class`() {
+        val copy = user.copy(password = "12345678")
+
+        println("user details: $user")
+        println("user copy: $copy")
+    }
+
+    @Test
+    fun `deconstruction`() {
+        val (username, password) = user
+        println("username: $username, password: $password")
     }
 }
