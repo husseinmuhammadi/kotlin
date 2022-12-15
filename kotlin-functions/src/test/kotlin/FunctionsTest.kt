4d69cf1e66588bb8f36922e824dd09cca110de7a
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-12-15 17:18:13 +0100

    primitive type array

--- kotlin-functions/src/test/kotlin/FunctionsTest.kt
+++ kotlin-functions/src/test/kotlin/FunctionsTest.kt
@@ -12,11 +12,24 @@ class FunctionsTest {
     fun `call function with variable number of arguments`() {
         val result = asList(items = *arrayOf("a", "b", "c"))
         println("$result")
+
+        val result2 = asList(1, 2, 3)
+        println("$result2")
+    }
+
+    /**
+     * If you want to pass a primitive type array into vararg, you need to convert it to a regular (typed) array using the toTypedArray() function
+     */
+    @Test
+    fun `primitive type array`() {
+        val a = intArrayOf(1, 2, 3) // IntArray is a primitive type array
+        val list = asList(-1, 0, *a.toTypedArray(), 4)
+        println("$list")
     }
 
     @Test
     fun `single-expression functions`() {
-        val result=increment(2)
+        val result = increment(2)
         assertEquals(3, result)
     }
 }
